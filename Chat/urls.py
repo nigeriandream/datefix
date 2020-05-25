046f2e57e52b1c39dfeef8dd1c9d3f8c0eebfc14
Louis <39104306+louieee@users.noreply.github.com>
2020-05-25 16:34:49 +0100

    Update chat url

--- Chat/urls.py
+++ Chat/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('<int:thread_id>/', chat, name='chatroom'),
+    path('', chat, name='chatroom'),
     path('all/', all_chats, name='all_chats'),
     path('match_<int:match_id>/choice/', request_, name="match_choice"),
     path('api/chat/<int:id_>/', get_chat, name="get_chat_api"),
