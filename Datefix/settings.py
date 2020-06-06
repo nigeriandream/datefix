07eea12192bb1ec36e577d4e67e01185ef91687f
Louieee <louis.paul9095@gmail.com>
2020-06-06 17:48:04 +0100

    add redis

--- Datefix/settings.py
+++ Datefix/settings.py
@@ -75,8 +75,7 @@ TEMPLATES = [
 CHANNEL_LAYERS = {"default": {
     "BACKEND": "channels_redis.core.RedisChannelLayer",
     "CONFIG": {
-        "hosts": [("localhost", 6379)]
-        #  "hosts": [os.environ.get('REDIS_URL', 'localhost'),6379]
+        "hosts": [(os.environ.get('REDIS_URL', 'localhost'),6379)]
         }
     
     }
