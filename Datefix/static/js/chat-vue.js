0036f80e46a90e9badc80ee14839a6d0feb8557a
Jennifer Echenim <floxzyjay@gmail.com>
2020-07-03 00:53:41 +0100

    consume API to create a chat thread

--- Datefix/static/js/chat-vue.js
+++ Datefix/static/js/chat-vue.js
@@ -1,45 +1,61 @@
 var app = new Vue({
-    delimiters: ["[[", "]]"],
-    el: "#app",
-    name: "chat",
-    data: {
-      messages: [],
-      chat_threads: [],
-      chat_messages: [],
-      chat_object: {},
-      chat_id: "",
-      message: "",
-      user: "",
-      id: "",
-      username: "",
-    },
-    async mounted() {
-      this.id = this.$refs.userID.value;
-      await this.getUser();
-      await this.getAllChats();
-      const url = window.location.href.replace("http", "ws");
-      this.socket = new WebSocket(url);
-      this.websocket()
-    },
+  delimiters: ["[[", "]]"],
+  el: "#app",
+  name: "chat",
+  data: {
+    messages: [],
+    chat_threads: [],
+    chat_messages: [],
+    chat_object: {},
+    chat_id: "",
+    message: "",
+    user: "",
+    id: "",
+    username: "",
+  },
+  async mounted() {
+    this.id = this.$refs.userID.value;
+    await this.createThread();
+    await this.getUser();
+    await this.getAllChats();
+    const url = window.location.href.replace("http", "ws");
+    this.socket = new WebSocket(url);
+    this.websocket();
+  },
   methods: {
     async websocket() {
       this.socket.onclose = (e) => {
         console.log("WebSocket Disconnected", e);
       };
-  
+
       this.socket.onopen = (e) => {
         console.log("WebSocket Connected", e);
         // this.socket.send(JSON.stringify({ chat_id: 1, username: "Louisane", function: "connect" }));
       };
-  
+
       this.socket.onerror = (e) => {
         console.log("WebSocket Error", e);
       };
-  
+
       this.socket.onmessage = (e) => {
         this.messages.push(JSON.parse(e.data));
         console.log("WebSocket received message", e);
       };
+    },
+    async createThread() {
+      try {
+        await fetch(`/chat/api/create/2`)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("thread>>>", data);
+            // if (data !== 'None'){
+            //   this.user = data;
+            //   this.username=data.username;
+            // }
+          });
+      } catch (error) {
+        console.log("error>>>", error);
+      }
     },
     async getUser() {
       try {
@@ -47,13 +63,13 @@ var app = new Vue({
           .then((response) => response.json())
           .then((data) => {
             console.log("user>>>", data);
-            if (data !== 'None'){
+            if (data !== "None") {
               this.user = data;
-              this.username=data.username;
-            }          
+              this.username = data.username;
+            }
           });
       } catch (error) {
-        console.log('error>>>', error);
+        console.log("error>>>", error);
       }
     },
     async getAllChats() {
@@ -65,11 +81,11 @@ var app = new Vue({
             this.chat_threads = data.chat_threads;
           });
       } catch (error) {
-        console.log('error>>>', error);
+        console.log("error>>>", error);
       }
     },
     async getSingleChat(chat) {
-      this.chat_object = chat
+      this.chat_object = chat;
       try {
         await fetch(`/chat/api/chat/${chat.chat_id}`)
           .then((response) => response.json())
@@ -78,18 +94,18 @@ var app = new Vue({
             this.chat_id = data.chat_id;
             this.chat_messages = data.chat_list;
           });
-          this.connect();
+        this.connect();
       } catch (error) {
-        console.log('error>>>', error);
+        console.log("error>>>", error);
       }
     },
     connect() {
       let chat_thread = {
         username: this.username,
         chat_id: this.chat_id,
-        function: 'connect'
-      }
-      console.log('chat_thread>>>', chat_thread);
+        function: "connect",
+      };
+      console.log("chat_thread>>>", chat_thread);
       this.socket.send(JSON.stringify(chat_thread));
     },
     sendMessage() {
@@ -97,14 +113,13 @@ var app = new Vue({
         sender_id: this.id,
         text: this.message,
         sender: this.username,
-        function: 'message'
+        function: "message",
       };
       this.messages.push(thread_obj);
       this.socket.send(JSON.stringify(thread_obj));
-      console.log('thread_obj>>>', thread_obj);
-      this.getSingleChat(this.chat_object)
+      console.log("thread_obj>>>", thread_obj);
+      this.getSingleChat(this.chat_object);
       this.message = "";
-    }
-  }
-  });
-  
+    },
+  },
+});
