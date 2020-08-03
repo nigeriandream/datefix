c4c7dcb03d24ff9cc8872facb8bc2239c1805fb7
Louis Ohaegbu <louis.paul9095@gmail.com>
2020-08-04 00:43:53 +0100

    fix verification sending

--- Datefix/settings.py
+++ Datefix/settings.py
@@ -101,9 +101,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 
 EMAIL_HOST_USER = 'mail.geldbaum@gmail.com'
 
