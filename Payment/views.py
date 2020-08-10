19db639e46d741d687e6025c3431a82fdd4bc6a2
Louis Ohaegbu <louis.paul9095@gmail.com>
2020-08-10 15:49:25 +0100

    match result fix

--- Payment/views.py
+++ Payment/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 
-
 # Create your views here.
 from Account.models import User
 
@@ -13,3 +12,15 @@ def payment(request):
         # pay money
         pass
     return render(request, 'Payment/payment.html')
+
+
+def test(request):
+    if request.method == 'GET':
+        if 'girls' in request.GET:
+            print(request.GET['girls'])
+            from django.http import HttpResponse
+            import json
+            return HttpResponse(json.dumps(str(request.GET['girls']).split(',')))
+        else:
+            return render(request, 'Payment/test.html')
+
