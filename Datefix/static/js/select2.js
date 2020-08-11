121fd711cebf0d696b2781292d5e34209a6b1fb7
Louis Ohaegbu <louis.paul9095@gmail.com>
2020-08-12 00:58:45 +0100

    minor fix

--- Datefix/static/js/select2.js
+++ Datefix/static/js/select2.js
@@ -2597,3 +2597,16 @@
 });
 
 
+const matches = document.getElementById('matches_')
+let match_list = []
+function select(self){
+    console.log(self.value)
+    if (! match_list.includes(self.value) && match_list.length < 2){
+        match_list.push(self.value)
+        self.setAttribute('selected', 'selected')
+    }else if (match_list.includes(self.value) && match_list.length <= 2){
+        match_list = match_list.filter((item)=>{return item !== self.value})
+        self.removeAttribute('selected')
+    }
+    matches.value = match_list.toString()
+}
