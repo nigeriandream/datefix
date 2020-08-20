29d623eb93e924bdf42a3b4e27ab386ce91af949
Louis Ohaegbu <louis.paul9095@gmail.com>
2020-08-20 17:27:07 +0100

    chat page responsive

--- Datefix/settings.py
+++ Datefix/settings.py
@@ -78,7 +78,7 @@ CHANNEL_LAYERS = {"default": {
     "BACKEND": "channels_redis.core.RedisChannelLayer",
     "CONFIG": {
         # "hosts": [os.environ.get('REDIS_URL', 'redis://redis:6379')],
-        "hosts": [('localhost', 6379)],
+        # "hosts": [('localhost', 6379)],
 
 
     }
