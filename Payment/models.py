bdff35767699fd61f2b9dd8a868e366b2c7530b6
Louis Ohaegbu <louis.paul9095@gmail.com>
2020-09-14 04:42:12 +0100

    add new migrations

--- Payment/models.py
+++ Payment/models.py
@@ -6,8 +6,8 @@ from Account.models import User
 
 class Payment(models.Model):
     payer = models.ForeignKey(User, on_delete=models.CASCADE)
-    package = models.CharField(choices=(('BASIC', 'BASIC'), ('CLASSIC', 'CLASSIC'), ('PREMIUM', 'PREMIUM')))
-    date_of_payment = models.DateTimeField()
-    status = models.CharField(choices=(('PENDING', 'PENDING'), ('PAID', 'PAID'), ('FAILED', 'FAILED')),
+    package = models.CharField(max_length=10, choices=(('BASIC', 'BASIC'), ('CLASSIC', 'CLASSIC'), ('PREMIUM', 'PREMIUM')), default='BASIC')
+    date_of_payment = models.DateTimeField(default=None)
+    status = models.CharField(max_length=10, choices=(('PENDING', 'PENDING'), ('PAID', 'PAID'), ('FAILED', 'FAILED')),
                               default='PENDING')
-    tx_ref = models.CharField(max_length=32)
+    tx_ref = models.CharField(max_length=32, null=True, blank=True)
